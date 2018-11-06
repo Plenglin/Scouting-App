540b2dfefc03bbeccdf0574e5a11e9c67c660e85
Maxim Yu <maximumpwnguin@gmail.com>
2018-11-06 08:40:23 -0800

    Treeviewnode

--- desktop/src/main/java/com/ironpanthers/scouting/desktop/view/ConnectionView.kt
+++ desktop/src/main/java/com/ironpanthers/scouting/desktop/view/ConnectionView.kt
@@ -3,10 +3,13 @@ package com.ironpanthers.scouting.desktop.view
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.ironpanthers.scouting.desktop.io.server.BluetoothServer
 import javafx.beans.property.SimpleBooleanProperty
+import javafx.beans.property.SimpleStringProperty
 import javafx.scene.Parent
+import javafx.scene.control.TreeItem
 import org.controlsfx.control.ToggleSwitch
 import org.slf4j.LoggerFactory
 import tornadofx.*
+import javax.bluetooth.RemoteDevice
 
 class ConnectionView : View() {
 
@@ -15,6 +18,7 @@ class ConnectionView : View() {
     private val logger = LoggerFactory.getLogger(javaClass)
     private val server = BluetoothServer()
     private val mapper = jacksonObjectMapper()
+    private val peers = observableList<DeviceConnection>()
 
     init {
         root = vbox {
@@ -26,11 +30,30 @@ class ConnectionView : View() {
                     action {
                         val wizard = find<PeerDiscoveryDialog>()
                         wizard.openModal(block = true)
-                        logger.info("Got device {}", wizard.result)
+                        val dev = wizard.result
+                        logger.info("Got device {}", dev)
+                        if (dev != null) {
+                            peers.add(DeviceConnection(dev))
+                        }
                         //logger.debug("Connecting to URL {}", wizard.result?.getConnectionURL(ServiceRecord.NOAUTHENTICATE_NOENCRYPT, false))
                     }
                 }
                 button("Refresh")
+            }
+            val rootTreeItem = TreeItem<String>()
+            rootTreeItem.isExpanded = true
+            rootTreeItem.children.bind(peers) { dev ->
+                TreeItem(dev.dev.bluetoothAddress).apply {
+                    userData = dev
+                    treeitem("Connect Chat Relay")
+                    treeitem("Connect Match Manager")
+                }
+            }
+            treetableview(rootTreeItem) {
+                isShowRoot = false
+                column<String, String>("Name") {
+                    it.value.valueProperty()
+                }
             }
         }
 
@@ -54,4 +77,9 @@ class ConnectionView : View() {
     private fun stopServer() {
         server.close()
     }
+}
+
+data class DeviceConnection(val dev: RemoteDevice) {
+    val nameProperty = SimpleStringProperty(dev.bluetoothAddress)
+    val isConnectedProperty = SimpleBooleanProperty(false)
 }
